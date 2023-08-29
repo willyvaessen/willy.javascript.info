9b7123fc945bc91898ef9e2b35fe67859137f5b4
willyvaessen <github@willyvaessen.nl>
2023-08-29 15:34:02 +0200

    Finished part 4. Variables

---
+++ 04.Variables/task_variables.js
@@ -0,0 +1,9 @@
+let admin;
+let name;
+alert("Step 1: Variable 'admin' & 'name' declared. With values " + admin + " & " + name);
+alert("Step 2: Value 'John' assigned to " + name + "variable 'name'.");
+name = "John";
+alert("The variable 'name' now has the value " + name);
+alert("The variable 'admin' now has the value " + admin);
+admin = name;
+alert("The variable 'admin' now has the value " + admin);
